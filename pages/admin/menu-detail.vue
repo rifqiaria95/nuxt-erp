9633cc27bc0e9fcc75e361e58c17ee08be7fa830
Rifqi Aria <rifqiaria95@outlook.com>
2025-07-05 18:30:15 +0700

    Fix: test fix delay modal menu detail 1.1

--- pages/admin/menu-detail.vue
+++ pages/admin/menu-detail.vue
@@ -199,6 +199,7 @@
                                         :options="menuGroups"
                                         label="name"
                                         :reduce="menuGroup => menuGroup.id"
+                                        :disabled="menuGroups.length === 0"
                                         placeholder="-- Pilih Menu Group --"
                                         class="select-menu-group"
                                     />   
